c0b6e5dd42f68f91c761073fe613ea8b375b2801
ICEworld27 <56315823+ICEworld27@users.noreply.github.com>
2021-07-27 19:57:23 +0500

    v2.2

--- mfTCPapp/mfTCPapp/Program.cs
+++ mfTCPapp/mfTCPapp/Program.cs
@@ -7,50 +7,48 @@ namespace mfTCPapp
 {
     class Program
     {
-        static public void Send(Socket soket, string msg)
+        static public void TrueSend(Socket socket, byte[] data)
         {
             int totalSent = 0;
-            string data = msg;
-            byte[] dataToSend = Encoding.ASCII.GetBytes(data);
-            byte[] lenOfsend = BitConverter.GetBytes(dataToSend.Length);
-            while (totalSent < 4)
+            while (totalSent < data.Length)
             {
-                int actuallySent = soket.Send(
-                lenOfsend,
+                int actuallySent = socket.Send(
+                data,
                 totalSent,
-                4 - totalSent,
-                SocketFlags.None
-                );
-                totalSent += actuallySent;
-            }
-            totalSent = 0;
-            while (totalSent < dataToSend.Length)
-            {
-                int actuallySent = soket.Send(
-                dataToSend,
-                totalSent,
-                dataToSend.Length - totalSent,
+                data.Length - totalSent,
                 SocketFlags.None
                 );
                 totalSent += actuallySent;
             }
         }
-        static public string Recive(Socket sock)
+        static public void TrueRecive(Socket socket, byte[] buffer)
         {
-            byte[] bufferLen = new byte[4];
-            sock.Receive(bufferLen);
-            byte[] buffer = new byte[BitConverter.ToInt32(bufferLen, 0)];
             int totalReceived = 0;
             while (totalReceived < buffer.Length)
             {
-                int actuallyReceived = sock.Receive(
-                    bufferLen,
+                int actuallyReceived = socket.Receive(
+                    buffer,
                     totalReceived,
                     buffer.Length - totalReceived,
                     SocketFlags.None
                     );
                 totalReceived += actuallyReceived;
             }
+        }
+        static public void Send(Socket soket, string msg)
+        {
+            string data = msg;
+            byte[] dataToSend = Encoding.ASCII.GetBytes(data);
+            byte[] lenOfsend = BitConverter.GetBytes(dataToSend.Length);
+            TrueSend(soket, lenOfsend);
+            TrueSend(soket, dataToSend);
+        }
+        static public string Recive(Socket sock)
+        {
+            byte[] bufferLen = new byte[4];
+            TrueRecive(sock, bufferLen);
+            byte[] buffer = new byte[BitConverter.ToInt32(bufferLen, 0)];
+            TrueRecive(sock, buffer);
             return Encoding.ASCII.GetString(buffer, 0, buffer.Length);
         }
         static string Change(string data)
